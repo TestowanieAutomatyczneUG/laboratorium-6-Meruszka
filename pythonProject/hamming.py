9360ea58476fc8905db345c0e1bea349ea657d18
Szymon Merski <merski70@gmail.com>
2021-11-18 00:34:46 +0100

    test 1

--- pythonProject/hamming.py
+++ pythonProject/hamming.py
@@ -1,8 +1,13 @@
 def distance(x: str, y: str):
-    x = x.split('')
-    y = y.split('')
-    suma = 0
-    for i in range(len(x)):
-        if x[i] == y[i]:
-            suma +=1
-    return suma
+    if len(x) == len(y):
+        if len(x) == 0 or len(y) == 0:
+            return 0
+        x = x.split('')
+        y = y.split('')
+        suma = 0
+        for i in range(len(x)):
+            if x[i] == y[i]:
+                suma +=1
+        return suma
+    else:
+        raise ValueError
