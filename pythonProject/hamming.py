4390b8989296cc9e744679037eda4392a286e7c7
Szymon Merski <merski70@gmail.com>
2021-11-18 00:37:25 +0100

    zadanie 1 zrobione

--- pythonProject/hamming.py
+++ pythonProject/hamming.py
@@ -2,12 +2,10 @@ def distance(x: str, y: str):
     if len(x) == len(y):
         if len(x) == 0 or len(y) == 0:
             return 0
-        x = x.split('')
-        y = y.split('')
         suma = 0
         for i in range(len(x)):
-            if x[i] == y[i]:
-                suma +=1
+            if x[i] != y[i]:
+                suma += 1
         return suma
     else:
-        raise ValueError
+        raise ValueError('Nie równe dane')
