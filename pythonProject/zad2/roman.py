1664a1e5630c2bf067d9543848082a9d33c7d1fa
Szymon Merski <merski70@gmail.com>
2021-11-18 20:15:12 +0100

    prawie prawie

--- pythonProject/zad2/roman.py
+++ pythonProject/zad2/roman.py
@@ -1,7 +1,11 @@
-def roman(x):
-    dic = {
-        "I": 1,
-        "II": 2,
-        "III": 3,
-        ""
-    }
+def roman(number):
+    conv = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+            [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+            [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'],
+            [1, 'I']]
+    result = ''
+    for denom, roman_digit in conv:
+
+        result += roman_digit * (number / denom)
+        number %= denom
+    return result
